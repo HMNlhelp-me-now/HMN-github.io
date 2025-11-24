0d47b1b825ef33eb06724b7e919b22544e01ff08
HMNlhelp-me-now <mvp00cu01@gmail.com>
2025-11-24 10:00:38 +0700

    firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,6 +1,6 @@
 // js/firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyA7pH4XYXYVTFV6wpiP9rUETsTFhQmPsWk",
+  
   authDomain: "mvpcu01-ebdea.firebaseapp.com",
   databaseURL: "https://mvpcu01-ebdea-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mvpcu01-ebdea",
@@ -9,8 +9,9 @@ const firebaseConfig = {
   appId: "1:347908782931:web:99f563d65c36fe6f9c3fa6",
   measurementId: "G-KZ2T5W9RSX"
 };
-h
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
  
+
 
